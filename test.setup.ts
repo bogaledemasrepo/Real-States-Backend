c54e51532a0b91d638e0bf8b59af5c15e85634b1
bogidemasrepo <bogidemas@gmail.com>
2026-01-01 12:36:22 +0300

    fix: match preload path in bunfig

--- test.setup.ts
+++
@@ -1,9 +0,0 @@
-import { afterAll } from 'bun:test';
-import db from './models/index';
-
-afterAll(async () => {
-  // Gracefully close the postgres-js connection pool
-  // This prevents the test runner from hanging
-  // @ts-expect-error - accessing the underlying client for postgres-js cleanup
-  await db.session.client.end();
-});
