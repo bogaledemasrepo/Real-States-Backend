ce4d919cd3aa9cc08a55b0a407bed30e0b7c0c20
bogidemasrepo <bogidemas@gmail.com>
2026-01-10 09:33:14 +0300

    server url update for versioning

--- app.ts
+++ app.ts
@@ -3,17 +3,20 @@ import swaggerUi from 'swagger-ui-express';
 import { specs } from './config/swagger';
 import authRoutes from './routes/auth.route';
 import propertyRoutes from './routes/property.route'
+import userRouter from './routes/users.route';
+import { protect } from './middleware/auth.middleware';
 const app = express();
 
 app.use(express.json());
 
 // Serve Swagger UI at /api-docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/property', propertyRoutes);
+app.use('/api/v1/users',protect, userRouter);
+app.use('/api/v1/property',protect, propertyRoutes);
 
-app.get('/health', (req, res) => {
+app.get('/api/v1/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import app from './app'; // Import the app you just shared
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}`);
-  console.log(`📄 Documentation: http://localhost:${PORT}/api-docs`);
-  console.log(`🏥 Health Check: http://localhost:${PORT}/health`);
+  console.log(`🚀 Server ready at http://localhost:${PORT}/api/v1`);
+  console.log(`📄 Documentation: http://localhost:${PORT}/api/v1/api-docs`);
+  console.log(`🏥 Health Check: http://localhost:${PORT}/api/v1/health`);
 });
